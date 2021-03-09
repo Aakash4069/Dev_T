1dd4df82cae4f833473ca166a6440bfec0f06d6e
aakash4069 <aakash4069@gmail.com>
2021-03-09 18:48:29 +0530

    request.js updated

--- 3_Web_Scarp_08_3_2021/raw/poc/request.js
+++ 3_Web_Scarp_08_3_2021/raw/poc/request.js
@@ -1,7 +1,7 @@
 let cheerio=require("cheerio");
 let request=require("request");
 console.log("before");
-request("httpssadfsaf://www.google.com/",cb);
+request("https://www.google.com/",cb);
 function cb(error,respose,html){
     console.log(error);
 }

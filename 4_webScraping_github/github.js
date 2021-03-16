c4d20def41035791f946c3c86ab30e473605d1df
aakash4069 <aakash4069@gmail.com>
2021-03-16 19:52:52 +0530

    topic name and its link

--- 4_webScraping_github/github.js
+++ 4_webScraping_github/github.js
@@ -5,16 +5,27 @@ let url="https://github.com/topics";
 request(url,cb);
 
 function cb(error,response,html){
+    if(error){
+        console.log(error);
+    }else{
+        githubtopics(html);
+    }
+    
+}
+function githubtopics(html){
+
     let $=cheerio.load(html);
 
     let topicsArray=$(".col-12.col-sm-6.col-md-4.mb-4 a");
 
     for(let i=0;i<topicsArray.length;i++){
-        let topic=$(topicsArray[i]);
-
-        let link=$(topic).attr("href");
+        let topicName=$(topicsArray[i]).text();
+        let link=$(topicsArray[i]).attr("href");
+        topicName=topicName.trim().split("\n")[0];
         let fulllink="https://www.github.com/"+link;
-        console.log(fulllink);
+        // console.log(topicName);
+
+        console.log(topicName+"  =>  "+fulllink);
         // console.log(err);
 
     }

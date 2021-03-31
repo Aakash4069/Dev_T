9681243861137c7d571d42565c168b78652d2532
aakash4069 <aakash4069@gmail.com>
2021-03-31 17:57:48 +0530

    warm up page topic href link

--- 4_Promises_Autom_23_03_2021/raw/poc/HK_code_submit.js
+++ 4_Promises_Autom_23_03_2021/raw/poc/HK_code_submit.js
@@ -24,22 +24,74 @@ browserOpenPromise.then(function(browser){
 }).then(function (){
     let submitbuttan=cTab.click("button[data-analytics='LoginPassword']");
     return submitbuttan;
-}).then(function() {
-    let waitForKit=cTab.waitForSelector(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled",{visible:true});
-    return waitForKit;
-
-}).then(function (){
-    let goToIPKit=cTab.click(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled");
-    return goToIPKit;
-
-}).then(function (){
-    let waitForWarmUp=cTab.waitForSelector("a[data-attr1='warmup']");
-    return waitForWarmUp;
-}).then(function (){
-    let clickonWarmUp=cTab.click("a[data-attr1='warmup']");
-    return clickonWarmUp;
-}).then(function (){
-    console.log("Reached warm up module");
-}).catch(function (err){
-    conmsole.log(err);
 })
+// .then(function() {
+//     let waitForKit=cTab.waitForSelector(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled",{visible:true});
+//     return waitForKit;
+
+// }).then(function (){
+//     let goToIPKit=cTab.click(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled");
+//     return goToIPKit;
+// })
+
+.then (function (){
+    let waitForKit=waitAndClick(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled");
+    return waitForKit;
+})
+// .then(function (){
+//     let waitForWarmUp=cTab.waitForSelector("a[data-attr1='warmup']");
+//     return waitForWarmUp;
+// }).then(function (){
+//     let clickonWarmUp=cTab.click("a[data-attr1='warmup']");
+//     return clickonWarmUp;
+// })
+.then (function (){
+    console.log("at warmup ");
+    let clickonWarmUp=waitAndClick("a[data-attr1='warmup']");
+    return clickonWarmUp;
+})
+.then(function (){
+    let waitForQuestions=cTab.waitForSelector("a[data-analytics='ChallengeListChallengeName']", { visible: true });
+    return waitForQuestions;
+})
+.then(function (){
+
+    function consoleWaleFn(){
+        let allElem=document.querySelectorAll("a[data-analytics='ChallengeListChallengeName']");
+        let linkArr=[];
+        for(let i=0;i<allElem.length;i++){
+            linkArr.push(allElem[i].getAttribute("href"));
+        }
+        return linkArr;
+    }
+    let linkArrProise=cTab.evaluate(consoleWaleFn);
+    return linkArrProise;
+
+})
+.then(function (linkArr){
+    console.log(linkArr);
+})
+.then(function (){
+    console.log("Reached warm up module");
+})
+.catch(function (err){
+    console.log(err);
+})
+
+function waitAndClick(Selector){
+
+    return new Promise(function (resolve,reject){
+
+        let waitFSelector=cTab.waitForSelector(Selector,{visible :true});
+        waitFSelector.then(function (){
+            
+            let clickToSelector=cTab.click(Selector);
+            return clickToSelector;
+        })
+        .then(function (){
+            resolve();
+        }).catch(function (err){
+            reject();
+        })
+    })
+}

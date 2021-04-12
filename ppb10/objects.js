3153ea09f288f90648ecea1098007c37bc3c3c49
aakash4069 <aakash4069@gmail.com>
2021-04-12 20:18:54 +0530

    access of  the object

--- ppb10/objects.js
+++ ppb10/objects.js
@@ -1,18 +1,18 @@
 
 
 
-let tony={
-    name:"Tony",
-    lastName:"Stark",
-    friends:['steve','Bruce','Peter'],
-    age:45,
-    isAvenger:true,
-    abc:undefined,
-    address:{
-        state:"New York",
-        city:"Long Island"
+let tony = {
+    name: "Tony",
+    lastName: "Stark",
+    friends: ['steve', 'Bruce', 'Peter'],
+    age: 45,
+    isAvenger: true,
+    abc: undefined,
+    address: {
+        state: "New York",
+        city: "Long Island"
     },
-    sayHi:function fn(){
+    sayHi: function fn() {
         console.log('I am iron man');
         return "part of journy is the end"
     },
@@ -26,11 +26,38 @@ let tony={
 // console.log(tony.address);
 // console.log(tony.address.city);
 
-// sscond methon
+// second methon to access object value 
 
-console.log(tony['name']);
-console.log(tony['friend']);
-console.log(tony['age']);
-console.log(tony['address']);
-console.log(tony['address']['city']);
-console.log(tony['sayHi']());
+// console.log(tony['name']);
+// console.log(tony['friends']);
+// console.log(tony['friends'][0]);
+// console.log(tony['age']);
+// console.log(tony['address']);
+// console.log(tony['address']['city']);
+// console.log(tony['sayHi']());
+
+
+// method to access to key only
+
+// method first 
+
+// let karr = Object.keys(tony);
+// console.log(karr);
+// method second
+// for(let k in tony){
+//     // console.log(tony.k);         this will not display keys value becouse k is not any key in the tony object
+
+//     // console.log(tony[k]);
+//     console.log(tony[k]);
+// }
+
+// third method
+
+let karr = Object.keys(tony);
+
+for (let i = 0; i < karr.length; i++) {
+    let key = karr[i];
+    console.log(tony[key]);
+}
+
+

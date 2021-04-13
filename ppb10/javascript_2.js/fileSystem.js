676d529d850dad5a0bf829c9834037d299251195
aakash4069 <aakash4069@gmail.com>
2021-04-13 19:22:57 +0530

    basic of node

--- ppb10/javascript_2.js/fileSystem.js
+++ ppb10/javascript_2.js/fileSystem.js
@@ -6,7 +6,7 @@ console.log(data);          //this give the output in the buffer format
 console.log(data.toString());   // this will convert he buffer formate to normal form
 
 let wdata = 'this is some demo data tha i want to write';
-fs.writeFileSync('./write.txt', wdata);        //this will first delete the content and write in the file
+fs.writeFileSync('write.txt', wdata);        //this will first delete the content and write in the file
 fs.appendFileSync('write.txt', 'my am in the best');     //this will append the value int the file
  // fs.unlinkSync('write.txt');                     // this will remove the file 
 // fs.mkdirSync("other");                              // this will create the file
